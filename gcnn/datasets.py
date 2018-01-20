54fd9d7674d3dbe2f7e8a2ae11947014f2216534
Teo Stocco <teo@zifeo.com>
2018-01-20 14:53:24 +0100

    start refactoring

--- gcnn/datasets.py
+++ gcnn/datasets.py
@@ -1,2 +1,18 @@
 import pandas as pd
+import numpy as np
+from torchvision import datasets
 
+
+def data_icebergs(set: str):
+    icebergs = pd.read_json('../data/{}.json'.format(set)).set_index('id')
+    icebergs = icebergs.assign(
+        inc_angle=pd.to_numeric(icebergs.inc_angle, 'coerce'),
+        band_1=icebergs.band_1.apply(np.array),
+        band_2=icebergs.band_2.apply(np.array),
+    )
+    return icebergs
+
+
+def mnist(train: bool):
+    x, y = zip(*datasets.MNIST('../data', train=train, download=True))
+    return pd.DataFrame(dict(x=x, y=y))
---
+++ gcnn/nets.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+
+from gcnn.layers import GraphChebyConv
+
+conv1_dim = 32
+conv2_dim = 64
+
+class Net1(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.gc1 = nn.Sequential(
+            # GraphFourierConv(f0.cuda() if cuda else f, 1, 10, bias=True),
+            GraphChebyConv(l0.cuda() if cuda else l0, 1, 10, 20, bias=True),
+            nn.ReLU(),
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(len(f0) * 10, 10),
+            nn.ReLU(),
+            nn.Softmax(1)
+        )
+
+    def forward(self, x):
+        out = self.gc1(x)
+        out = self.fc(out.view(out.size(0), -1))
+        return out
+
+class Net2(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.gc1 = nn.Sequential(
+            # GraphFourierConv(l0.cuda() if cuda else l0, 1, conv1_dim, bias=True),
+            GraphChebyConv(l0.cuda() if cuda else l0, 1, conv1_dim, 25, bias=True),
+            nn.ReLU(),
+            nn.MaxPool1d(4),
+        )
+
+        self.gc2 = nn.Sequential(
+            # GraphFourierConv(l2.cuda() if cuda else l2, conv1_dim, conv2_dim, bias=True),
+            GraphChebyConv(l2.cuda() if cuda else l2, conv1_dim, conv2_dim, 25, bias=True),
+            nn.ReLU(),
+            nn.MaxPool1d(4),
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(len(f2) // 4 * conv2_dim, 512),
+            nn.ReLU(),
+            # nn.Dropout(0.5),
+            nn.Linear(512, 10),
+            nn.ReLU(),
+            nn.Softmax(1)
+        )
+
+    def forward(self, x):
+        out = self.gc1(x)
+        out = self.gc2(out)
+        out = self.fc(out.view(out.size(0), -1))
+        return out
