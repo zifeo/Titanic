ff6998ca94dfdc351241712e9fc2ec5d8fee5ac6
Teo Stocco <teo@zifeo.com>
2018-01-20 14:11:02 +0000

    extract dataset

--- gcnn/datasets.py
+++ gcnn/datasets.py
@@ -3,7 +3,7 @@ import numpy as np
 from torchvision import datasets
 
 
-def data_icebergs(set: str):
+def load_icebergs(set: str):
     icebergs = pd.read_json('../data/{}.json'.format(set)).set_index('id')
     icebergs = icebergs.assign(
         inc_angle=pd.to_numeric(icebergs.inc_angle, 'coerce'),
@@ -13,6 +13,6 @@ def data_icebergs(set: str):
     return icebergs
 
 
-def mnist(train: bool):
+def load_mnist(train: bool):
     x, y = zip(*datasets.MNIST('../data', train=train, download=True))
     return pd.DataFrame(dict(x=x, y=y))
