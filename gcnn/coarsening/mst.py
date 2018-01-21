31148ae9d10e60af82a132db6774500dbd0c95eb
Teo Stocco <teo@zifeo.com>
2018-01-21 13:00:48 +0000

    small graph example with mst and kron

--- gcnn/coarsening/mst.py
+++ gcnn/coarsening/mst.py
@@ -7,7 +7,7 @@ import numpy as np
 def calc_weight(n, root, pred, local_tree):
     
     if n==root:
-        return 0, root
+        return 0, root, root
     
     parent = pred[root, n]
 
@@ -23,35 +23,45 @@ def calc_weight(n, root, pred, local_tree):
         
     w = 2./(1./w_p + 1./w_d)
 
-    return w, gparent
+    return w, parent, gparent
 
 def mst(graph, levels=2):
     
     G = [graph]
     
     for _ in range(levels):
-        test_dist_triu = np.triu(graph)
-        Tree = minimum_spanning_tree(test_dist_triu)
-        Tree = Tree + Tree.T
-
-        local_tree = Tree.todense()
-        distance_matrix, pred = dijkstra(local_tree, directed=False, unweighted=True, return_predecessors=True)
-
-        root = np.random.choice(np.arange(distance_matrix.shape[0]))
-        even_nodes = distance_matrix[:, root] % 2 == 0
-        even_nodes = np.arange(graph.shape[0])[even_nodes]
-
-        weight_tree = np.zeros(local_tree.shape)
-        
-
-        for n in even_nodes:
-            print("node : ",n)
-            out = calc_weight(n, root, pred, local_tree)
-            print("out : ", out)
-            weight_tree[n, out[1]] = out[0]
-            weight_tree[out[1], n] = out[0]
-
-        graph = weight_tree
+        graph, _ = one_level_MST(graph)
         G.append(graph)
         
     return G
+
+def one_level_MST(test_dist):
+
+    test_dist_triu = np.triu(test_dist)
+    Tree = minimum_spanning_tree(test_dist_triu)
+    Tree = Tree + Tree.T
+    local_tree = Tree.todense()
+
+    distance_matrix, pred = dijkstra(Tree.todense(), directed=False, unweighted=True, return_predecessors=True)
+
+    root = np.random.choice(np.arange(distance_matrix.shape[0]))
+    even_nodes = distance_matrix[:, root] % 2 == 0
+    even_nodes = np.arange(test_dist.shape[0])[even_nodes]
+
+    weight_tree = np.zeros((int(local_tree.shape[0]/2), int(local_tree.shape[1]/2)))
+    
+    array = [-1]*local_tree.shape[0]
+    cluster = 0
+    
+    for n in even_nodes:
+        new_weight, parent, gparent = calc_weight(n, root, pred, local_tree)
+        array[n] = cluster
+        array[parent] = cluster
+        cluster += 1
+        
+        n = int(n/2)
+        out_div = int(gparent/2)
+        weight_tree[n, out_div] = new_weight
+        weight_tree[out_div, n] = new_weight
+        
+    return weight_tree, array
